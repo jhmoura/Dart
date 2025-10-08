9c38297df9c692c5c9e6982a1c6d5a8710c9225f
jhmoura <juliohmoura@pm.me>
2025-10-08 15:55:58 -0300

    Funções em Dart

--- bin/app.dart
+++ bin/app.dart
@@ -1,7 +1,51 @@
 import 'dart:io';
 
 void main() {
-  print('Olá, meu nome é Dart. Qual se nome?: ');
-  var entrada = stdin.readLineSync();
-  print("Muito prazer, $entrada. Vamos fazer vários programas juntos!");
+  double n1 = double.parse(stdin.readLineSync()!);
+  double n2 = double.parse(stdin.readLineSync()!);
+
+  void soma() {
+    print(n1 + n2);
+  }
+
+  void subtracao() {
+    print(n1 - n2);
+  }
+
+  void multiplicacao() {
+    print(n1 * n2);
+  }
+
+  void divisao() {
+    print(n1 / n2);
+  }
+
+  soma();
+  subtracao();
+  multiplicacao();
+  divisao();
 }
+
+//Variáveis primitivas
+/*
+int numeroInteiro = 0;
+double numeroDecimal = 33,14;
+String texto = "Texto";
+bool isAtivo = true;
+
+**Variáveis compostas
+List<String> nomes = ["Ana", "João", "Maria"];
+Map<String, String> agenda = {"Ana": "1234-5678", "João": "9876-5432"};
+Set<int> numerosUnicos = {1, 2, 3, 4, 3}; // O número 3 só aparece uma vez
+
+**Acessando valores
+print("Número inteiro: $numeroInteiro");
+print("Número decimal: $numeroDecimal");
+print("Texto: $texto");
+print("Ativo: $isAtivo");
+
+print("Primeiro nome da lista: ${nomes[0]}");
+print("Telefone da Ana: ${agenda['Ana']}");
+print("Números únicos no conjunto: $numerosUnicos");
+
+*/
